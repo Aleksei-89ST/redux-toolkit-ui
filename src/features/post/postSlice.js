be10ce464afec0c071275e0840335a474836b689
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-26 15:53:45 +0300

    логика удаления posts

--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -5,10 +5,17 @@ const initialState = {
   posts: [],
 };
 export const getPosts = createAsyncThunk(
-  "posts/getPosts/",
-  async (_, { rejectWithValue,dispatch }) => {
+  "posts/getPosts",
+  async (_, { rejectWithValue, dispatch }) => {
     const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-    dispatch(setPosts(res.data))
+    dispatch(setPosts(res.data));
+  }
+);
+export const deletePostById = createAsyncThunk(
+  "posts/deletePostById",
+  async (id, { rejectWithValue, dispatch }) => {
+    await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    dispatch(deletePost(id));
   }
 );
 
@@ -16,16 +23,22 @@ export const postSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-      setPosts: (state , action) => {
-          state.posts = action.payload
-      }
+    setPosts: (state, action) => {
+      state.posts = action.payload;
+    },
+    deletePost: (state, action) => {
+      state.posts = state.posts.filter((post) => post.id !== action.payload);
+    },
   },
-  extraReducers: { 
-      [getPosts.fulfilled]: () => console.log('fullfiled'),
-      [getPosts.pending]: () => console.log('pending'),
-      [getPosts.rejected]: () => console.log('rejected'),
+  extraReducers: {
+    [getPosts.fulfilled]: () => console.log("getPosts:fullfiled"),
+    [getPosts.pending]: () => console.log("getPosts:pending"),
+    [getPosts.rejected]: () => console.log("getPosts:rejected"),
+    [deletePostById.fulfilled]: () => console.log("deletePost:fullfiled"),
+    [deletePostById.pending]: () => console.log("deletePost:pending"),
+    [deletePostById.rejected]: () => console.log("deletePost:rejected"),
   },
 });
 
-export const {setPosts} = postSlice.actions
-export default postSlice.reducer
+export const { setPosts, deletePost } = postSlice.actions;
+export default postSlice.reducer;
