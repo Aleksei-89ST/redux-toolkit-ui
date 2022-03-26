3207596db5396940f64888ab9a31804791cbcc8e
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-26 15:18:09 +0300

    описал логику для запроса API

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { getPosts } from '../features/post/postSlice'
 import PostItem from './PostItem'
 
 const Posts = () => {
+    const dispatch = useDispatch()
     return (
         <div>
             <button
                 type='submit'
                 className='bg-lime-300  hover:bg-lime-400 transition-all p-2 text-sm'
+                onClick={() => (getPosts)}
             >
                 Get posts
             </button>
